fdb1730caa7b122e8d04141143596338d282a655
Malanius Privierre <malaniusprivierre@gmail.com>
2024-01-08 22:06:20 +0100

    feat(iot): create devices policy

--- stacks/IoT.stack.ts
+++ stacks/IoT.stack.ts
@@ -1,5 +1,6 @@
-import * as iotActions from '@aws-cdk/aws-iot-actions-alpha';
-import * as iot from '@aws-cdk/aws-iot-alpha';
+import * as iotAlphaActions from '@aws-cdk/aws-iot-actions-alpha';
+import * as iotAlpha from '@aws-cdk/aws-iot-alpha';
+import * as iot from 'aws-cdk-lib/aws-iot';
 import { Function, Queue, StackContext } from 'sst/constructs';
 
 export function IoT({ app, stack }: StackContext) {
@@ -9,13 +10,32 @@ export function IoT({ app, stack }: StackContext) {
 
   const iotDlq = new Queue(stack, 'IoTDlq', {});
 
-  new iot.TopicRule(stack, 'TopicRule', {
-    sql: iot.IotSql.fromStringAsVer20160323(
-      `SELECT * FROM '${app.name}/${app.stage}/status'`
+  const topicName = `${app.name}/${app.stage}/status`;
+  new iotAlpha.TopicRule(stack, 'TopicRule', {
+    sql: iotAlpha.IotSql.fromStringAsVer20160323(
+      `SELECT * FROM '${topicName}'`
     ),
-    actions: [new iotActions.LambdaFunctionAction(iotFunction)],
-    errorAction: new iotActions.SqsQueueAction(iotDlq.cdk.queue, {
+    actions: [new iotAlphaActions.LambdaFunctionAction(iotFunction)],
+    errorAction: new iotAlphaActions.SqsQueueAction(iotDlq.cdk.queue, {
       useBase64: false,
     }),
+  });
+
+  new iot.CfnPolicy(stack, 'Policy', {
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Effect: 'Allow',
+          Action: 'iot:Connect',
+          Resource: `arn:aws:iot:${stack.region}:${stack.account}:client/ToiletClient`,
+        },
+        {
+          Effect: 'Allow',
+          Action: 'iot:Publish',
+          Resource: `arn:aws:iot:${stack.region}:${stack.account}:topic/${topicName}`,
+        },
+      ],
+    },
   });
 }
